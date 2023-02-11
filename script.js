d5fca620468310ef48dfbf9fdb626ea8a189bbcf
carloskirui541 <123838356+carloskirui541@users.noreply.github.com>
2023-02-11 15:22:36 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+class Pizza {
+	constructor(pizzaType, pizzaSize) {
+		this.type = pizzaType;
+		this.size = pizzaSize;
+		this.crust = 'original';
+	}
+	bake() {
+		console.log(
+			`Baking a ${this.size} ${this.type} ${this.crust} crust pizza.` 
+		)
+	}
+}
